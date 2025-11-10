e1f98de506a1c5e837b0cd0e8a0147e2801021a0
MrMaxBoi <maxhaohean@gmail.com>
2025-11-10 15:24:38 +0800

    clean up npm modules

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
-import { LiquidGlass } from '@wxperia/liquid-glass-vue'
 
 const app = createApp(App)
 const pinia = createPinia()
@@ -11,5 +10,4 @@ const pinia = createPinia()
 app.use(pinia)
 app.use(vuetify)
 app.use(router)
-app.component('LiquidGlass', LiquidGlass)
 app.mount('#app')
