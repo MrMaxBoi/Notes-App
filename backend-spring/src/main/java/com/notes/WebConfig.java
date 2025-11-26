5d027a435026b86f5d181d4c179fc3aad3bc321b
MrMaxBoi <maxhaohean@gmail.com>
2025-11-26 15:15:26 +0800

    added webconfig.java

---
+++ backend-spring/src/main/java/com/notes/WebConfig.java
@@ -0,0 +1,20 @@
+package com.notes;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // Forward all non-API routes to index.html for Vue.js routing
+        registry.addViewController("/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/**/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+                .setViewName("forward:/index.html");
+    }
+}
